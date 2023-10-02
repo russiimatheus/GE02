4bab49bf90aef12f4c4ecd64761bb6adac272edd
Matheus Abrantes <mabrante@uccs.edu>
2023-10-02 15:49:53 -0600

    Finished GE03 updates

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Portfolio, Student, Project  # Ensure you import Project here
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Portfolio)
+admin.site.register(Project)  # Register the Project model
+
